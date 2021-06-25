5f02b66fd1b91abec2526af1cc6f6ef17da73e37
mikemcwilliam23 <mjmcwilliam@outlook.com>
2021-06-25 13:11:00 -1000

    new thing

--- image_data.R
+++ image_data.R
@@ -17,52 +17,28 @@ dat$preview<-small
 dat$date<-substr(dat$preview,1, 10)
 dat$date<-gsub("_", "/", dat$date)
 dat$year<-substr(dat$date,1, 4)
-dat
+dat$LOC <- substr(dat$image_path,nchar(dat$image_path)-6, nchar(dat$image_path)-4)
 
-dat$location<-c(
-"Port Phillip Bay",
-"Port Phillip Bay",
-"Port Phillip Bay",
-"Port Phillip Bay",
-"Whitsundays",
-"Whitsundays",
-"Coral Sea",
-"Maldives",
-"Maldives",
-"Lizard",
-"Great Barrier Reef",
-"Great Barrier Reef",
-"Kauai",
-"Heron Is.",
-"Heron Is.",
-"Komodo",
-"Coral Sea",
-"Vicoria, Australia",
-"Vicoria, Australia",
-"Coral Sea",
-"Coral Sea",
-'Coral Sea',
-"Coral Sea",
-"Crescent Head, NSW",
-"Palau",
-"Palau",
-"Palau",
-"Palau",
-"Palau",
-"Palau",
-"Palau",
-"Haleiwa",
-"CA",
-"Catalina",
-"Catalina",
-"Catalina",
-"Makapu'u",
-"Kona",
-"Kona",
-"Haleiwa",
-"Kona",
-"Kona"
-)
+# 3 letter name code (don't use x)
+dat$location<-NA
+dat$location[dat$LOC=="PPB"]<-"Port Phillip Bay, Australia"
+dat$location[dat$LOC=="WHT"]<-"Whitsundays, Australia"
+dat$location[dat$LOC=="NGB"]<-"Northern GBR, Australia"
+dat$location[dat$LOC=="MAL"]<-"Maldives"
+dat$location[dat$LOC=="LIZ"]<-"Lizard Is., Australia"
+dat$location[dat$LOC=="CRL"]<-"Coral Sea, Australia"
+dat$location[dat$LOC=="CGB"]<-"GBR, Australia"
+dat$location[dat$LOC=="KAW"]<-"Kauai, HI"
+dat$location[dat$LOC=="HER"]<-"Heron Is., Australia"
+dat$location[dat$LOC=="KMO"]<-"Komodo, Indonesia"
+dat$location[dat$LOC=="NSW"]<-"New South Whales, Australia"
+dat$location[dat$LOC=="VIC"]<-"Victoria, Australia"
+dat$location[dat$LOC=="PAL"]<-"Palau"
+dat$location[dat$LOC=="CAL"]<-"California, USA"
+dat$location[dat$LOC=="CAT"]<-"Catalina Island, USA"
+dat$location[dat$LOC=="OAH"]<-"Oahu, HI"
+dat$location[dat$LOC=="HAL"]<-"Haleiwa, HI"
+dat$location[dat$LOC=="KON"]<-"Kona, HI"
 
 
 dat
